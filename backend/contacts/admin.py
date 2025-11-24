cf03e65c0b646d2f761673fd050d316784cd9846
rcc00n <vadrud2016@gmail.com>
2025-11-24 02:45:17 -0700

    Contact messages working

--- backend/contacts/admin.py
+++ backend/contacts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import QuoteRequest
+from .models import ContactMessage, QuoteRequest
 
 
 @admin.register(QuoteRequest)
@@ -8,3 +8,10 @@ class QuoteRequestAdmin(admin.ModelAdmin):
     list_display = ("name", "phone", "email", "fulfillment", "created_at")
     search_fields = ("name", "phone", "email", "message")
     list_filter = ("fulfillment", "created_at")
+
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "phone", "created_at")
+    search_fields = ("name", "email", "phone", "message")
+    date_hierarchy = "created_at"
