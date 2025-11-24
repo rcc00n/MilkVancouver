fadbcfb0ae9205f65d0863652ec61d9ad05960c5
rcc00n <vadrud2016@gmail.com>
2025-11-24 03:33:24 -0700

    Serve frontend from backend image

--- backend/shop/urls.py
+++ backend/shop/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.http import JsonResponse
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.generic import TemplateView
 
 
 def health_check(_request):
@@ -15,4 +16,6 @@ urlpatterns = [
     path("api/", include("contacts.urls")),
     path("api/", include("blog.urls")),
     path("health/", health_check),
+    # Serve the SPA for non-API routes
+    re_path(r"^(?!admin/|api/|static/|health/).*$", TemplateView.as_view(template_name="index.html")),
 ]
