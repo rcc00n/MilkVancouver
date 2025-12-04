b425ae6bd6a6c55b25c5c11ff6b99c2e4cbf2c12
rcc00n <vadrud2016@gmail.com>
2025-12-04 16:41:27 -0700

    fix

---
+++ backend/products/migrations/0005_merge_20251204_0000.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("products", "0003_product_is_active"),
+        ("products", "0004_productimage_file"),
+    ]
+
+    operations = []
